1764043ace2e5d392f091a9c4854ba61584b4d60
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-05-05 22:26:07 +0700

    feat(the-essentials): reviewing about setup projcet

--- the-essentials/public/app.js
+++ the-essentials/public/app.js
@@ -3,4 +3,13 @@ const container = ReactDOM.createRoot(root);
 const Header = () => {
   return /*#__PURE__*/React.createElement("h1", null, "Cara buat komponent");
 };
-container.render(/*#__PURE__*/React.createElement(Header, null));
+function MainContent() {
+  return /*#__PURE__*/React.createElement("p", null, " ", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laboriosam corrupti tenetur quaerat fuga illum aliquid praesentium sit minus officia eum, asperiores fugit veniam consequuntur velit magni numquam ab pariatur suscipit aperiam? Dolores ab quam reiciendis rerum corporis eos est.");
+}
+const Footer = () => {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("footer", null, "copy; right by ", /*#__PURE__*/React.createElement("b", null, "Fajar saputra"), " made 2025"));
+};
+function App() {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Header, null), /*#__PURE__*/React.createElement(MainContent, null), /*#__PURE__*/React.createElement(Footer, null));
+}
+container.render(/*#__PURE__*/React.createElement(App, null));
--- the-essentials/src/app.js
+++ the-essentials/src/app.js
@@ -1,11 +1,36 @@
-const root = document.getElementById("root")
-const container = ReactDOM.createRoot(root)
+const root = document.getElementById("root");
+const container = ReactDOM.createRoot(root);
 
 const Header = () => {
+    return <h1>Cara buat komponent</h1>;
+};
+
+function MainContent() {
     return (
-        <h1>Cara buat komponent</h1>
+        <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laboriosam corrupti tenetur quaerat fuga illum aliquid praesentium sit minus officia eum, asperiores fugit veniam consequuntur
+            velit magni numquam ab pariatur suscipit aperiam? Dolores ab quam reiciendis rerum corporis eos est.
+        </p>
+    );
+}
+
+const Footer = () =>{
+    return (
+        <>
+     <footer>copy; right by <b>Fajar saputra</b> made 2025</footer>
+        </>
     )
 }
 
+function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer/>
+        </>
+    );
+}
 
-container.render(<Header/>)
+container.render(<App />);
