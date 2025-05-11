ca2449c38ae291ff8380c02a11d6d58dbc325d6b
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-05-11 22:27:22 +0700

    feat(to-do-list): add new project about to do list

---
+++ to-do-list/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Tasks from "./components/Tasks";
+
+
+function App() {
+
+
+  return (
+    <>
+    <Tasks task={"testing"} />
+    </>
+  )
+}
+
+export default App
---
+++ to-do-list/src/assets/components/Tasks.jsx
@@ -0,0 +1,6 @@
+
+export default function Tasks({task}) {
+    return (
+        <li>{ task}</li>
+    )
+}
