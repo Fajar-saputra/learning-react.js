3f07df8f20e529a81a93a7db19233e958849702d
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-06-25 22:42:43 +0700

    feat(project-web-rpl): add component payment page

--- project-web-rpl/src/App.jsx
+++ project-web-rpl/src/App.jsx
@@ -3,11 +3,12 @@ import HomePage from "./pages/HomePage";
 import ToursPage from "./pages/ToursPage";
 import UmkmPage from "./pages/umkmPage";
 import KafePage from "./pages/KafePage";
-import HotelPage from './pages/HotelPage';
+import HotelPage from "./pages/HotelPage";
 import Footer from "./pages/Footer";
 import ServicesPage from "./pages/ServicesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
+import PaymentPage from "./pages/PaymentPage";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
                     <Route path="/services" element={<ServicesPage />} />
                     <Route path="/hotel" element={<HotelPage />} />
                     <Route path="/kafe" element={<KafePage />} />
+                    <Route path="payment" element={<PaymentPage />} />
                     <Route path="/Footer" element={<Footer />} />
                 </Routes>
             </div>
---
+++ project-web-rpl/src/pages/PaymentPage.jsx
@@ -0,0 +1,5 @@
+export default function PaymentPage() {
+    return (
+        <p>Ini halaman payment</p>
+    )
+}
