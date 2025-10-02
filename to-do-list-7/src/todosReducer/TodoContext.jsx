cbd96960160c8a9929ddda3983e9109d7f3493a9
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-10-02 23:25:11 +0700

    feat(to-do-list-7): reviewing about context and reducer

--- to-do-list-7/src/todosReducer/TodoContext.jsx
+++ to-do-list-7/src/todosReducer/TodoContext.jsx
@@ -5,28 +5,73 @@ import { v4 } from "uuid";
 export function TodosReducer(todos, action) {
     switch (action.type) {
         case "ADD_TODO":
-            return [...todos, { id: v4(), title: action.payload.title,done: false, tasks: [] }];
-        
-        case "DELETE_TODO":
-            return todos.filter(todo => todo.id !== action.payload.todoId);
-        
-        case "DONE_TODO":
-            return todos.map(todo => todo.id === action.payload.todoId ? { ...todo, done: !todo.done } : todo);
-        
-        case "UPDATE_TODO":
-            return todos.map(todo => todo.id === action.payload.todoId? {...todo, title: action.payload.text}: todo)
+            return [...todos, { id: v4(), title: action.payload.title, done: false, tasks: [] }];
 
-            default:
-                todos;
+        case "DELETE_TODO":
+            return todos.filter((todo) => todo.id !== action.payload.todoId);
+
+        case "DONE_TODO":
+            return todos.map((todo) => (todo.id === action.payload.todoId ? { ...todo, done: !todo.done } : todo));
+
+        case "UPDATE_TODO":
+            return todos.map((todo) => (todo.id === action.payload.todoId ? { ...todo, title: action.payload.text } : todo));
+        
+        case "ADD_TASK":
+            return todos.map(todo => todo.id === action.payload.todoId ? {
+                ...todo, tasks: [...todo.tasks, {id: v4(), title: action.payload.title, done: false}]
+            }: todo)
+
+        default:
+            todos;
     }
 }
 
 // initial todos
 const initialTodos = [
-    { id: v4(), title: "Belajar React", done: false ,tasks: [] },
-    { id: v4(), title: "Belajar React", done: false ,tasks: [] },
-    { id: v4(), title: "Belajar React", done: false ,tasks: [] },
-    { id: v4(), title: "Belajar React", done: false ,tasks: [] }
+    {
+        id: v4(),
+        title: "Belajar React Level 1",
+        done: false,
+        tasks: [
+            { id: v4(), title: "Belajar React Level 2", done: false },
+            { id: v4(), title: "Belajar Python Level 2", done: false },
+            { id: v4(), title: "Belajar C Level 2", done: false },
+            { id: v4(), title: "Belajar PHP Level 2", done: false },
+        ],
+    },
+    {
+        id: v4(),
+        title: "Belajar React Level 1",
+        done: false,
+        tasks: [
+            { id: v4(), title: "Belajar React Level 2", done: false },
+            { id: v4(), title: "Belajar Python Level 2", done: false },
+            { id: v4(), title: "Belajar C Level 2", done: false },
+            { id: v4(), title: "Belajar PHP Level 2", done: false },
+        ],
+    },
+    {
+        id: v4(),
+        title: "Belajar React Level 1",
+        done: false,
+        tasks: [
+            { id: v4(), title: "Belajar React Level 2", done: false },
+            { id: v4(), title: "Belajar Python Level 2", done: false },
+            { id: v4(), title: "Belajar C Level 2", done: false },
+            { id: v4(), title: "Belajar PHP Level 2", done: false },
+        ],
+    },
+    {
+        id: v4(),
+        title: "Belajar React Level 1",
+        done: false,
+        tasks: [
+            { id: v4(), title: "Belajar React Level 2", done: false },
+            { id: v4(), title: "Belajar Python Level 2", done: false },
+            { id: v4(), title: "Belajar C Level 2", done: false },
+            { id: v4(), title: "Belajar PHP Level 2", done: false },
+        ],
+    },
 ];
 
 // create context
@@ -34,7 +79,7 @@ export const TodosContext = createContext(null);
 
 // provider
 export default function TodosProvider({ children }) {
-    const  [todos, dispatch]  = useReducer(TodosReducer, initialTodos);
+    const [todos, dispatch] = useReducer(TodosReducer, initialTodos);
 
     return <TodosContext.Provider value={{ todos, dispatch }}>{children}</TodosContext.Provider>;
 }
--- to-do-list-7/src/todosReducer/TodoItem.jsx
+++ to-do-list-7/src/todosReducer/TodoItem.jsx
@@ -3,19 +3,110 @@ import { TodosContext } from "./TodoContext";
 
 export default function TodoItem() {
     const { todos, dispatch } = useContext(TodosContext);
-    const [updateTodo, setUpdateTodo] = useState(""); 
+    // todo
+    const [editTodoId, setEditTodoId] = useState(null);
+    const [editTodo, setEditTodo] = useState("");
+
+    // task
+    const [task, setTask] = useState("");
+
+    // style input
+    const styleInput = {
+        padding: ".5rem",
+        width: "250px",
+    };
+
+    // func todo
+    const handleClickEditTodo = (todoId, title) => {
+        setEditTodo(title);
+        setEditTodoId(todoId);
+    };
+
+    const handleSaveEditTodo = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: "UPDATE_TODO",
+            payload: { todoId: editTodoId, text: editTodo },
+        });
+
+        handleCancelEditTodo();
+    };
+
+    const handleCancelEditTodo = () => {
+        setEditTodo("");
+        setEditTodoId(null);
+    };
+
+    // func task
+    const handleAddTask = (e, todoId) => {
+        e.preventDefault();
+        dispatch({
+            type: "ADD_TASK",
+            payload: { todoId, title: task[todoId] },
+        });
+
+        setTask((prev) => ({ ...prev, [todoId]: "" }));
+    };
 
     return (
         <div>
             {todos.map((todo) => (
                 <div key={todo.id}>
-                        {todo.done? <span style={{color: "red"}}>{todo.title}</span> : <span>{todo.title}</span> }
-                    
-                    <span>
-                        <button type="button" onClick={() => dispatch({type: "DONE_TODO", payload: {todoId: todo.id}})}>✔</button>
-                        <button type="button" onClick={() => dispatch({type: "UPDATE_TODO", payload: {todoId: todo.id, text: updateTodo}})}>⚙</button>
-                        <button type="button" onClick={() => dispatch({type: "DELETE_TODO", payload: {todoId: todo.id}})}>🗑</button>
-                    </span>
+                    {todo.id === editTodoId ? (
+                        <div>
+                            <form action="" onSubmit={handleSaveEditTodo}>
+                                <input type="text" value={editTodo} onChange={(e) => setEditTodo(e.target.value)} />
+                                <button type="submit">✔</button>
+                                <button type="button" onClick={handleCancelEditTodo}>
+                                    ✖
+                                </button>
+                            </form>
+                        </div>
+                    ) : (
+                        <div>
+                            {todo.done ? <span style={{ color: "red" }}>{todo.title}</span> : <span>{todo.title}</span>}
+
+                            <span>
+                                <button type="button" onClick={() => dispatch({ type: "DONE_TODO", payload: { todoId: todo.id } })}>
+                                    ✔
+                                </button>
+                                <button type="button" onClick={() => handleClickEditTodo(todo.id, todo.title)}>
+                                    ⚙
+                                </button>
+                                <button type="button" onClick={() => dispatch({ type: "DELETE_TODO", payload: { todoId: todo.id } })}>
+                                    🗑
+                                </button>
+                            </span>
+
+                            <form onSubmit={(e) => handleAddTask(e, todo.id)} style={styleInput}>
+                                <input type="text" value={task[todo.id] || ""} placeholder="New task..." onChange={(e) => setTask({ ...task, [todo.id]: e.target.value })} />
+                                <button type="submit">✔</button>
+                            </form>
+
+                            <div>
+                                <ul>
+                                    {todo.tasks.map((task) => (
+                                        <li key={task.id}>
+                                            <div>
+                                                {task.done ? <span style={{ color: "green" }}>{task.title}</span> : <span>{task.title}</span>}
+                                                <span>
+                                                    <button type="button" onClick={() => dispatch({ type: "DONE_TODO", payload: { todoId: todo.id } })}>
+                                                        ✔
+                                                    </button>
+                                                    <button type="button" onClick={() => handleClickEditTodo(todo.id, todo.title)}>
+                                                        ⚙
+                                                    </button>
+                                                    <button type="button" onClick={() => dispatch({ type: "DELETE_TODO", payload: { todoId: todo.id } })}>
+                                                        🗑
+                                                    </button>
+                                                </span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
